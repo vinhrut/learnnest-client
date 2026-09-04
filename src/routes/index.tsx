172d64c75db51fd2b0bbedfa8f24ba111eaa7fa5
vanhungn <vanhungnvh1712004@gmail.com>
2026-09-04 10:46:51 +0700

    conflict

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,6 @@ import { AppLayout } from '@/components/layout/AppLayout';
 import { AuthLayout } from '@/components/layout/AuthLayout';
 import { LoginPage } from '@/pages/auth/LoginPage';
 import { UserListPage } from '@/pages/users/UserListPage';
-import { Comment } from '@/components/comments/comment';
-import { ChatBox } from '@/components/chatBoxs/chatBoxs';
 import { RoleRoute } from './RoleRoute';
 import { RoleHome } from './RoleHomeRedirect';
 import { RouteError } from './RouteError';
@@ -87,10 +85,6 @@ export const router = createBrowserRouter([
           { path: '/ba/tasks', element: <BATaskPage /> },
           { path: '/ba/profile', element: <ProfilePage /> },
         ],
-      },
-      {
-        path: "/comments",
-        element: <ChatBox />
       },
       { path: '/403', element: <Forbidden /> },
 
